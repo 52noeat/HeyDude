594872319b485897ed9293750a208115378c9202
dongjinc <whsso1101@ajou.ac.kr>
2019-11-17 20:47:11 +0900

    change html to ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Main Page
 router.get('/', (req, res) => {
-    res.render('../views/home.html');
+    res.render('../views/home.ejs');
+});
+router.get('/signin', (req, res) => {
+    res.render('../views/signin.ejs');
+});
+router.get('/signup', (req, res) => {
+    res.render('../views/signup.ejs');
 });
 
 module.exports = router;
